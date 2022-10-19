d864c0815b18a9c64374b04b2b04aa0049332445
Arif Bishwas <60783337+arifbiswas@users.noreply.github.com>
2022-10-20 01:47:46 +0600

    semi completed

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000
 
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 
 app.use(cors())
 
@@ -13,6 +14,29 @@ app.get('/',(req,res)=>{
 
 app.get('/categories',(req,res)=>{
     res.send(categories);
+})
+
+app.get('/category/:id',(req,res)=>{
+    const id =req.params.id;
+
+    if(id === '08'){
+        res.send(news)
+    }
+    else{
+
+        const category_news = news.filter(n => n.category_id === id )
+        res.send(category_news)
+    }
+})
+
+app.get('/news',(req,res)=>{
+    res.send(news)
+})
+
+app.get('/news/:id',(req,res)=>{
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id) 
+    res.send(selectedNews);
 })
 
 app.listen(port , ()=>{
